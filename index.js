06dbc92c9cc0b873b06816e2981ced850d2b6ae5
Daniel Schoch <d.schoch90@gmail.com>
2018-08-01 19:56:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var beatles = [];
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    beatles.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return beatles;
+}
