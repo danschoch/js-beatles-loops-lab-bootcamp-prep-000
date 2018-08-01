c75a5aefd8f63e920c3fab085b8010c549fde85d
Daniel Schoch <d.schoch90@gmail.com>
2018-08-01 20:22:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,9 @@ function johnLennonFacts (facts) {
 
 function iLoveTheBeatles (n) {
   var arr = [];
-  
+  do {
+    arr.push("I love the Beatles!");
+    n++
+  } while (n < 15);
+  return arr;
 }
